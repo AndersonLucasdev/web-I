b23f642cd85c5e13b5d5bbc98221f3b5a5f03f2a
anderson <andinlucas8@gmail.com>
2023-10-21 13:58:23 -0300

    FEAT/ terminando lógica do botão

--- test2/index.js
+++ test2/index.js
@@ -1,9 +1,16 @@
-const PegarInformacoes = () => {
-  var ValorEntrada = document.getElementsByClassName("entrada").value();
+function calcularIMC(genero) {
+    const nome = document.querySelector(".entradanome").value;
+    const altura = parseFloat(document.querySelector(".entradaaltura").value);
+    const peso = parseFloat(document.querySelector(".entradapeso").value);
+    const informacoes = document.querySelector(".informacoes");
 
-  document.getElementById('resultado').textContent = "Informações: " + ValorEntrada;
-};
-
-document
-  .getElementsByClassName("BotaoMasculino")
-  .addEventListener("click", PegarInformacoes());
+    if (!nome || !altura || !peso || !genero) {
+        informacoes.innerHTML = "<p>Existem campos vazios ou você não selecionou o gênero.</p>";
+    } else {
+        if (genero === "masculino") {
+            console.log("Masculino");
+        } else if (genero === "feminino") {
+            console.log("Feminino");
+        }
+    }
+}
